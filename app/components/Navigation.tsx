7ed1fe9b37417039a04a567b4b8b7fd705afa39b
mi3765 <chips1225@outlook.jp>
2024-01-29 20:41:55 +0900

    initial-commit

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+
+const Navigation = () => {
+	return (
+		<header className="border-b py-5">
+			<div className="container max-w-screen-xl mx-auto relative flex justify-center items-center">
+				<Link href="/" className="font-bold text-xl cursor-pointer">
+					mi3765-Blog
+				</Link>
+			</div>
+			<div className="absolute right-5">
+				<div className="flex space-x-4">
+					<Link href="/auth/profile">Profile</Link>
+					<Link href="/auth/login">Login</Link>
+					<Link href="/auth/signup">SignUp</Link>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Navigation;
